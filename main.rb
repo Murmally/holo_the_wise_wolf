e8a38dd70eb6d11ef6e71f3c107d1f8368339d8f
Murmally <Murmally@seznam.cz>
2020-02-14 02:05:03 +0100

    Refactoring
    Improved code quality

--- main.rb
+++ main.rb
@@ -1,16 +1,12 @@
-# TODO: formatovani helpu (Embed)
-# TODO: logovani zpusobu pouzivani bota
 # TODO: presunout globalni promenne a perzistentni data do statickych trid
 # TODO: strawpoll
 # TODO: perhaps
 
-
 require 'discordrb'
 require 'logger'
 
-OWNER_ID = 328207275758059520
-Bajkar = 358337749000126471
 
+$bot = Discordrb::Commands::CommandBot.new token: XXX, prefix: 'h!'
 file = File.open('log.txt', File::WRONLY | File::APPEND)
 $log = Logger.new(file, formatter: proc{ |severity, datetime, progname, msg| 
     "#{severity} #{datetime} || #{msg}\n"
@@ -21,23 +17,43 @@ $personal_log = Logger.new(personal_file, formatter: proc{|severity, datetime, p
     "#{datetime} || #{msg}\n"
 })
 
-$quote = "Default quote, use `h!set_quote` to add your own!"
+
+
+# To avoid repeatedly searching DC servers' emojis
+class SavedEmojis
+	attr_reader :hahaa, :coolStory
+
+	def initialize
+		@hahaa = $bot.find_emoji("HAhaa")
+		@coolStory = $bot.find_emoji("CoolStory")
+		puts "SavedEmojis initialized"
+	end
+end
+
+# Used to keep track of certain individuals' IDs
+class UserIDs
+	def self.owner
+		return 328207275758059520
+	end
+
+	def self.bajkar
+		return 358337749000126471
+	end
+end
+
+
 
 $last_copypasta = ""
 $last_gde_body = Time.now - 60
-$last_rozsazeni_time = Time.now
 
-$coolStory = ""
-$hAhaa = ""
-$star = ""
 
-bot.ready() do |event|
-  $coolStory = bot.find_emoji("CoolStory")
-  $hAhaa = bot.find_emoji("HAhaa")
-  $star = bot.find_emoji("star")
+
+$bot.ready() do |event|
+	$EmojiManager = SavedEmojis.new
+	$UserIdManager = UserIDs.new
 end
 
-bot.command(:help) do |event|
+$bot.command(:FIT_help) do |event|
   event << "`h!get_definition [SUBJECT] [term]` => get definition, eg. `h!get_definition ISA Tok`"
   event << "`h!add_definition [SUBJECT] [term] [definition...]` => add definition to a subject (Helper/Mod only)"
   event << "`h!add_subject [SUBJECT]` => add a subject to list of subjects, eg. `h!add_subject IZP` (Helper/Mod only)"
@@ -47,11 +63,11 @@ bot.command(:help) do |event|
   event << "h!FIT_zaznamy_link"
   event << "h!FIT_video_link\n"
 
-  event << "h!help_random => list of several useless (although mildly entertaining) commands"
+  event << "h!help => list of several useless (although mildly entertaining) commands"
 end
 
 
-bot.command(:help_random) do |event|
+$bot.command(:help) do |event|
   event << "h!pasta => get random copypasta"
   event << "h!pastas => list of all copypastas"
   event << "h!set_quote => set this bot's quote"
@@ -59,17 +75,20 @@ bot.command(:help_random) do |event|
   event << "h!why_ruby => the decision-making behind choosing Ruby\n"
   event << "h!karma"
   event << "h!cbt"
-  event << "h!ga"
   event << "h!exams"
   event << "h!linux"
   event << "h!racism"
   event << "h!flip"
+  event << "h!JonLajoie"
+  event << "h!somebody"
+  event << "h!gender"
 end
 
-bot.command(:big_boi_help) do |event|
-  if event.author.id != OWNER_ID
+$bot.command(:big_boi_help) do |event|
+  if event.author.id != UserIdManager.owner
     return nil
   end
+  event << "h!windmill"
   event << "h!whitelist"
   event << "h!JonLajoie"
   event << "h!bajkalarka"
@@ -77,10 +96,10 @@ bot.command(:big_boi_help) do |event|
   event << "h!set_log"
   event << "h!kybl"
   event << "h!plna_taska"
-  event << "h!rod"
+  event << "h!gender"
 end
 
-bot.reaction_add(emoji: "\u2b50") do |event|
+$bot.reaction_add(emoji: "\u2b50") do |event|
   return nil
   if Discordrb::Channel.load_message(event.message.id)
     puts "ayy"
@@ -99,14 +118,14 @@ bot.reaction_add(emoji: "\u2b50") do |event|
 end
 
 # TODO
-bot.command(:blacklist) do |event, user|
+$bot.command(:blacklist) do |event, user|
   whitelist = File.read("./whitelist.txt")
   id = get_userid_from_tag(user)
   
 end
 
-bot.command(:whitelist) do |event, user|
-  if event.author.id != OWNER_ID
+$bot.command(:whitelist) do |event, user|
+  if event.author.id != UserIdManager.owner
     event.respod "Not permitted"
     return nil
   end
@@ -116,7 +135,7 @@ bot.command(:whitelist) do |event, user|
   event.respond "User has been added to whitelist"
 end
 
-bot.command(:get_definition) do |event, subject, name|
+$bot.command(:get_definition) do |event, subject, name|
   begin
     event.respond File.read("./school/#{subject}/#{name}.txt")
   rescue
@@ -124,7 +143,7 @@ bot.command(:get_definition) do |event, subject, name|
   end
 end
 
-bot.command(:add_definition, usage: 'add_definition [subject] [name] [args]') do |event, subject, name, *args|
+$bot.command(:add_definition, usage: 'add_definition [subject] [name] [args]') do |event, subject, name, *args|
   authorized_roles = ["Mod", "Helper"]
   if !check_for_role_by_rolenames(event.author, authorized_roles)
     $log.info create_log_message("add_definition_attempt", event)
@@ -143,7 +162,7 @@ bot.command(:add_definition, usage: 'add_definition [subject] [name] [args]') do
   end
 end
 
-bot.command(:add_subject) do |event, subject|
+$bot.command(:add_subject) do |event, subject|
   if Dir.exists? "./school/#{subject}"
     event.respond "Subject already exists"
     return nil
@@ -153,7 +172,7 @@ bot.command(:add_subject) do |event, subject|
   end
 end
 
-bot.command(:subject_definitions) do |event, subject|
+$bot.command(:subject_definitions) do |event, subject|
   msg = ""
   if !Dir.exists? "./school/#{subject}"
     event.respond "No such subject found, try h!subjects"
@@ -168,7 +187,7 @@ bot.command(:subject_definitions) do |event, subject|
 end
 
 
-bot.command(:subjects) do |event|
+$bot.command(:subjects) do |event|
   dirs = Dir.glob("**/school/*").select {|f| File.directory? f}
   dirs.each do |file|
     event << "#{file.split("/")[-1]}"
@@ -176,22 +195,22 @@ bot.command(:subjects) do |event|
   nil
 end
 
-bot.command(:FIT_zaznamy_link) do |event|
+$bot.command(:FIT_zaznamy_link) do |event|
   $log.info create_log_message("FIT_zaznamy_link", event)
   event.respond "https://drive.google.com/drive/folders/1YtEOsXAUEZDzh5WwAmml9g8FT7iHgWcG"
 end
 
-bot.command(:FIT_video_link) do |event|
+$bot.command(:FIT_video_link) do |event|
   $log.info create_log_message("FIT_video_link", event)
   event.respond "video1.fit.vutbr.cz"
 end
 
 
-bot.command(:somebody) do |event|
+$bot.command(:somebody) do |event|
   event.respond "<https://youtu.be/L_jWHffIx5E?t=36>"
 end
 
-bot.command(:diag) do |event|
+$bot.command(:diag) do |event|
   $log.info create_log_message("diag", event)
   if test_pastas
     event.respond "Copypasta library seems to be OK!"
@@ -200,55 +219,51 @@ bot.command(:diag) do |event|
   end
 end
 
-bot.message(from: ["Bajkař", "ElDandee"]) do |event|
-  event.message.create_reaction($coolStory)
-  event.message.create_reaction($hAhaa)
+$bot.message(from: ["Bajkař", "ElDandee"]) do |event|
+  event.message.create_reaction($EmojiManager.coolStory)
+  event.message.create_reaction($EmojiManager.hahaa)
 end
 
-bot.command(:flip) do |event|
+$bot.command(:flip) do |event|
   num = rand 0..1
   event.respond num == 0 ? "Ano" : "Ne"
 end
 
-bot.command(:quote) do |event|
-  $log.info create_log_message("quote", event)
-  event.respond $quote
+$bot.command(:quote) do |event|
+	event.respond File.read("./quote.txt")
 end
 
-bot.command(:set_quote) do |event|
-  $log.info create_log_message("set_quote", event)
-  new_quote = ""
-  words = event.content.split(' ')
-  for i in 1...words.length
-    new_quote += words[i] + " "
-  end
-  new_quote += "– *#{event.author.name}*"
-  $quote = new_quote
-  event.respond "Quote set to \"#{new_quote}\""
+$bot.command(:set_quote) do |event, *args|
+	quote_file = File.new("./quote.txt", "w")
+	args.each do |arg|
+		quote_file.write("#{arg} ")
+	end
+	quote_file.close
+	event.respond "Quote set!"
 end
 
-bot.command(:bajkalarka) do |event|
+$bot.command(:bajkalarka) do |event|
   event.respond "=== UVOD ===\n\n-by Bajkar"
 end
 
-bot.command(:karma) do |event|
+$bot.command(:karma) do |event|
   $log.info create_log_message("karma", event)
   msg = "Sice nejsi nejkrásnější, ani nejchytřejší, ani dobrý sportovec, ale my tě máme stejně rádi :heart:\n"
   event.respond msg
 end
 
-bot.command(:racism) do |event|
+$bot.command(:racism) do |event|
   $log.info create_log_message("racism", event)
   event.respond "Racism bad mmmkay"
 end
 
-bot.command(:ping) do |event|
+$bot.command(:ping) do |event|
   $log.info "ping || #{Time.now - event.timestamp} || #{event.author.name} || #{event.content}"
   event.respond "#{Time.now - event.timestamp} s"
 end
 
-bot.command(:set_log) do |event, new_log|
-  if event.author.id != OWNER_ID
+$bot.command(:set_log) do |event, new_log|
+  if event.author.id != UserIdManager.owner
     return nil
   end
   $log.info create_log_message("new_log", event)
@@ -259,7 +274,7 @@ bot.command(:set_log) do |event, new_log|
   nil
 end
 
-bot.command(:pastas) do |event|
+$bot.command(:pastas) do |event|
   msg = ""
   content = Dir["./copypastas/**.txt"]
   content.each do |file|
@@ -268,7 +283,7 @@ bot.command(:pastas) do |event|
   event.respond msg
 end
 
-bot.command(:pasta, max_args: 2) do |event, *args|
+$bot.command(:pasta, max_args: 2) do |event, *args|
   $log.info create_log_message("pasta", event)
   if args.length == 0
     event.respond get_random_pasta
@@ -277,92 +292,66 @@ bot.command(:pasta, max_args: 2) do |event, *args|
   end
 end
 
-bot.message(start_with: "!kasparek") do |event|
+$bot.message(start_with: "!kasparek") do |event|
   event.respond "Neposlouchej MEE6, i s tvým kašpárkem se dá zahrát ... mírně nadprůměrné divadlo"
 end
 
-bot.message(contains: [/gde body/i, /body gde/i, ":GDEBODY:"]) do |event|
+$bot.message(contains: [/gde ?body/i, /body ?gde/i, ":GDEBODY:"]) do |event|
   if $last_gde_body + 5 < Time.now
     $log.info create_log_message("gde body", event)
     event.respond "Pal do piče"
   end
 end
 
-bot.command(:why_ruby) do |event|
+$bot.command(:why_ruby) do |event|
   $log.info create_log_message("why_ruby", event)
   event.respond "Ruby is a big dick language and here's why: <https://www.youtube.com/watch?v=dQw4w9WgXcQ>"
 end
 
-bot.command(:exams) do |event|
+$bot.command(:exams) do |event|
   $log.info create_log_message("exams", event)
   event.respond File.read("./copypastas/exams.txt")
 end
 
-# TODO
-# bot.command(:strawpoll) do |event, question, *options|
-#   $log.info create_log_message("strawpoll", event)
-#   msg = "#{question}:\n"
-
-#   i = 0  
-#   emoji_nums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'keycap_ten']
-#   row = ""
-#   for emoji in emoji_nums
-#     row = ":#{emoji}:\t#{options[i]}\n"
-#     i += 1
-#     if i > options.length
-#       break
-#     else
-#       msg = msg.concat(row)
-#       #new_reaction = 
-#     end
-#   end
-#   event.respond(msg)
-# end
-
-bot.command(:cbt) do |event|
+$bot.command(:cbt) do |event|
   $log.info create_log_message("cbt", event)
   event.respond File.read("./copypastas/cbt.txt")
 end
 
-bot.command(:ga) do |event|
-  $log.info create_log_message("gibabay", event)
-  event.respond "GI:b:A:b:AY WHEN"
-end
-
-bot.command(:linux) do |event|
+$bot.command(:linux) do |event|
   $log.info create_log_message("linux interjection", event)
   event.respond File.read("./copypastas/gnu_interjection.txt")
 end
 
-bot.command(:kybl) do |event|
+$bot.command(:kybl) do |event|
   event.respond "<https://www.youtube.com/watch?v=ovolaH4gMOk>"
 end
 
-bot.message(contains: /hol[ao]/i) do |event|
+$bot.message(contains: /hol[ao]/i) do |event|
   puts "Someone mentioned you! || server: #{event.server.name}, in: #{event.channel.name}, from: #{event.author.name} => #{event.content}"
   $personal_log.info "server: #{event.server.name}, in: #{event.channel.name}, from: #{event.author.name} => #{event.content}"
 end
 
-bot.command(:JonLajoie) do |event|
-  event << "h!idgaf"
-  event << "h!eng"
-  event << "h!eng2"
-  event << "h!ikp"
+$bot.command(:JonLajoie) do |event|
+  event << "h!idgaf => Fuck Everything"
+  event << "h!eng => Everyday Normal Guy"
+  event << "h!eng2 => Everyday Normal Guy 2"
+  event << "h!ikp => I Kill People"
 end
 
-bot.command(:ikp) do |event|
+$bot.command(:ikp) do |event|
   event.respond "https://www.youtube.com/watch?v=xC03hmS1Brk"
 end
 
-bot.command(:idgaf) do |event|
+$bot.command(:idgaf) do |event|
   event.respond "https://www.youtube.com/watch?v=ulIOrQasR18"
 end
 
-bot.command(:eng) do |event|
+$bot.command(:eng) do |event|
   event.respond "https://www.youtube.com/watch?v=5PsnxDQvQpw"
 end
 
-bot.command(:eng2) do |event|
+$bot.command(:eng2) do |event|
   event.respond "https://www.youtube.com/watch?v=GmG4X9PGOXs"
 end
 
@@ -425,32 +414,42 @@ def test_pastas
   return res
 end
 
-bot.message(contains: /dobrou noc holo/i) do |event|
-  if event.author.id != OWNER_ID
+
+$bot.message(contains: /dobrou noc holo/i) do |event|
+  if event.author.id != UserIdManager.owner
     return
   end
   event.respond "Dobrou noc"
   exit
 end
 
-bot.command(:ruby) do |event|
-  event.respond "Hóvno"
+$bot.message(contains: [/thank you holo/i, /thanks holo/i, /d[ií]ky holo/i, /d[eě]kuj[iu] holo/i]) do |event|
+	event.message.react("❤️")
 end
 
-bot.command(:plna_taska) do |event|
+
+$bot.command(:plna_taska) do |event|
   event.respond "<https://www.youtube.com/watch?v=5xQdOshDUOw>"
 end
 
-bot.command(:rod) do |event|
-  event.respond "Jsem žena!"
+$bot.command(:gender) do |event|
+  event.respond "I am a beautiful and strong female!"
 end
 
-bot.message(contains: /kdyby se mi cht[eě]lo/i) do |event|
+$bot.message(contains: /kdyby se mi cht[eě]lo/i) do |event|
   event.respond "jak se ti nechce"
 end
 
-bot.message(contains: /nem[aá]m pravdu\?/i) do |event|
-  if event.author.id == OWNER_ID  # in case it's me
+$bot.message(contains: [/am i right\??/i, /amirite\??/i]) do |event|
+	if rand(0..1) == 0
+		event.respond "Yes, you are right."
+	else
+		event.respond "No, you are wrong."
+	end
+end
+
+$bot.message(contains: /nem[aá]m pravdu\??/i) do |event|
+  if event.author.id == UserIdManager.owner  # in case it's me
     if event.content.match? (/nemám pravdu?/i)
       event.respond "Ne, nemáš pravdu."
     else
@@ -466,7 +465,7 @@ bot.message(contains: /nem[aá]m pravdu\?/i) do |event|
 end
 
 # example
-bot.command(:random, min_args: 0, max_args: 2, usage: 'random [min/max] [max]') do  |event, min, max|
+$bot.command(:random, min_args: 0, usage: 'random [min/max] [max]') do  |event, min, max|
   if max
     rand(min.to_i..max.to_i)
   elsif min
@@ -476,4 +475,4 @@ bot.command(:random, min_args: 0, max_args: 2, usage: 'random [min/max] [max]') 
   end
 end
 
-bot.run
+$bot.run
